2d06c39d8e60c498631d1fd80500c964a05c967e
CI <username@users.noreply.github.com>
2023-11-21 08:44:47 +0100

    feat: added fellowship base export

--- fellowship.ts
+++ fellowship.ts
@@ -18,3 +18,141 @@ export type fellowship = {
   contact: string
   details: fellowshipDetails
 }
+
+export default {
+  source: "gql",
+  perPage: {
+    options: [9, 12, 16],
+    default: 9,
+  },
+  type: null, // 'directory' | 'file' | null
+  path: null, // path to the folder where the content is stored
+  create: true, // allow to create new items
+  filters: {
+    year: {
+      type: "Select",
+      rules: {},
+      label: "year",
+      items: [],
+    } /* 
+      categories: {
+        type: 'TextInput',
+        rules: {},
+        label: 'Search',
+      },
+      author: {
+        type: 'Autocomplete',
+        rules: {},
+        label: 'authors',
+      }, */,
+  },
+  sort: {
+    // sort options
+    nameasc: {
+      // by name from a to z
+      icon: "sort-alphabetical-ascending",
+      text: "by-name-from-a-to-z",
+      value: ["article_title", 1],
+    },
+    namedesc: {
+      // by name from z to a
+      icon: "sort-alphabetical-descending",
+      text: "by-name-from-z-to-a",
+      value: ["article_title", -1],
+    },
+    dateasc: {
+      // by date from most recent to oldest
+      icon: "sort-calendar-descending",
+      text: "by-date-most-recent-first",
+      value: ["date", -1],
+      default: true,
+    },
+    datedesc: {
+      // by date from oldest to most recent
+      icon: "sort-calendar-ascending",
+      text: "by-date-oldest-first",
+      value: ["date", 1],
+    },
+  },
+  views: {
+    rows: {
+      icon: "view-list",
+      default: true,
+    },
+    tiles: {
+      name: "tiles",
+      icon: "view-quilt",
+    },
+    grid: {
+      name: "grid",
+      icon: "view-day",
+    },
+  },
+  schema: {
+    /* firstname: string
+    lastname: string
+    affiliations: [{ affiliation: affiliation; positions: [position] }]
+    picture: image
+    socials: socials
+    biography: string
+    consent: consent
+    groups: groups */
+    firstname: {
+      label: "firstname",
+      component: "TextField",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: "",
+      description: "",
+      hint: false,
+      rules: {
+        required: true,
+        min: 5,
+        max: 200,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "firstname", // item type on schema.org
+    },
+    lastname: {
+      label: "lastname",
+      component: "TextField",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: "",
+      description: "",
+      hint: false,
+      rules: {
+        required: true,
+        min: 5,
+        max: 200,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "lastname", // item type on schema.org
+    },
+    consent: {
+      label: "consent",
+      component: "SimpleObjectWrapper",
+      type: 3, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: "",
+      description: "",
+      hint: false,
+      rules: {
+        required: true,
+        min: 5,
+        max: 200,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "consent", // item type on schema.org
+    },
+  },
+}
--- fellowshipDetails.ts
+++ fellowshipDetails.ts
@@ -11,3 +11,111 @@ export type fellowshipDetails = {
   selectionProcess: string // Dates, arbitrages, etc.
   researchSupport: string // cf les moyens de la recherche
 }
+
+export default {
+  schema: {
+    data: {
+      label: "data",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description: "data consent (display info about me)",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "data", // item type on schema.org
+    },
+    record: {
+      label: "record",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description: "record consent (record my activity, droit à l'image)",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "record", // item type on schema.org
+    },
+    diffusion: {
+      label: "diffusion",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description:
+        "broadcast consent (broadcast my activity, diffusion de contenus)",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "diffusion", // item type on schema.org
+    },
+    publication: {
+      label: "publication",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description: "publication consent (publish my content, generate DOIs)",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "publication", // item type on schema.org
+    },
+    email: {
+      label: "email",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description: "email communications (including newsletter)",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "email", // item type on schema.org
+    },
+    newsletter: {
+      label: "newsletter",
+      component: "Checkbox",
+      type: 0, // 0 = primitive, 1 = object, 2 = array, 3 = template
+      default: false,
+      description: "",
+      hint: false,
+      rules: {
+        required: true,
+      },
+      visibility: {
+        default: true, // same as hidden = true
+        switchIf: [], // array of conditions to switch the visibility, each condition will be assessed as a boolean
+        disjonctive: false, // if true, show only if one of the if is true, if false, show only if all of the if are true
+      },
+      meta: "newsletter", // item type on schema.org
+    },
+  },
+}
