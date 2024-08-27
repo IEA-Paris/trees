676800b3a1349f76fe21609ca7dfe26d1ae2c244
Hamed Bouare <hamed.bouare@paris-iea.fr>
2024-08-27 15:27:59 +0200

    feat: added modulesSate export

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import fs from "fs"
+import path from "path"
 import { ModuleType } from "./lib/generate"
 import Model from "./src/model"
 import { Sort, Views } from "./src/list"
@@ -86,5 +88,36 @@ const configData: Record<string, ConfigValue> = {
   relatedPeople: defaultConfigRelatedPeople,
 }
 
+const distDir = path.resolve("./dist")
+
+const getJsonFiles = (directory: string): string[] => {
+  return fs
+    .readdirSync(directory)
+    .filter((file) => path.extname(file) === ".json")
+    .map((file) => path.join(directory, file))
+}
+
+const jsonFiles = getJsonFiles(distDir)
+
+const modulesState: Record<string, any> = {}
+
+const readJsonFile = (filePath: string): any => {
+  try {
+    const data = fs.readFileSync(filePath, "utf8")
+    return JSON.parse(data)
+  } catch (err) {
+    console.error(`Error reading file ${filePath}:`, err)
+  }
+}
+
+jsonFiles.forEach((filePath) => {
+  const content = readJsonFile(filePath)
+  if (content) {
+    const fileName = path.basename(filePath, ".json")
+    modulesState[fileName] = content
+  }
+})
+
 export { configData }
 export type { Form, Sort, Views, ConfigValue, Model, ModuleType }
+export { modulesState }
