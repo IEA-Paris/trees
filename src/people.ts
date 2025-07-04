fb62aa8e58f28505547bf6bbf5c90ac7face6eb3
Billybobbonnet <boitalettre9@gmail.com>
2025-07-04 09:04:37 +0200

    fix: vintage filter name

--- src/people.ts
+++ src/people.ts
@@ -37,7 +37,7 @@ const defaultConfig: Model = {
       groups: {
         type: "Select",
       },
-      "groups.vintage": {
+      vintage: {
         type: "Select",
         visibility: {
           default: false, // same as hidden = true
